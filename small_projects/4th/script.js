b244edaab52b19fcde7f714e795daf5a72f899f2
Aditya Kumar <aditya.kumar@indianexpress.com>
2022-12-18 17:30:10 +0530

    changes

--- small_projects/4th/script.js
+++ small_projects/4th/script.js
@@ -1,12 +1,20 @@
 const messageInput = document.getElementById("message-input");
 
-messageInput.addEventListener("keydown", function(event) {
-    if(event.key == "Enter") {
-        getMessage();
-    }
-})
+messageInput.addEventListener("keydown", function (event) {
+  if (event.key == "Enter") {
+    getMessage();
+  }
+});
+
+messageInput.addEventListener("focus", function (event) {
+  event.target.style.background = "lightgrey";
+});
+
+messageInput.addEventListener("blur", function (event) {
+  event.target.style.background = "";
+});
 
 function getMessage() {
-    document.getElementById("message-output").innerHTML = messageInput.value;
-    messageInput.value = "";
+  document.getElementById("message-output").innerHTML = messageInput.value;
+  messageInput.value = "";
 }
